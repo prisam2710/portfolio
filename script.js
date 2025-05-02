b143943a3277432745f36243b016842938e4f292
Priyanshu Samantaray <priyanshusanantaray1729@gmail.com>
2025-05-02 08:42:45 +0530

    Added Dark Mode and modified styles

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
     alert("Welcome to Priyanshu's Portfolio!");
   });
-  
+  const toggleBtn = document.getElementById("darkModeToggle");
+
+// Load saved mode from localStorage
+if (localStorage.getItem("darkMode") === "enabled") {
+  document.body.classList.add("dark");
+  toggleBtn.textContent = "☀️ Light Mode";
+}
+
+// Toggle function
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("darkMode", "enabled");
+    toggleBtn.textContent = "☀️ Light Mode";
+  } else {
+    localStorage.setItem("darkMode", "disabled");
+    toggleBtn.textContent = "🌙 Dark Mode";
+  }
+});
